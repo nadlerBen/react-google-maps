dee45d0c4cb90b22d63eb8e6d03c336946a97996
bennadler <bennadlerr@gmail.com>
2021-02-14 12:39:52 +0200

    added google map view component

---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { getLocation } from '../utils/ipdata';
+
+const GoogleMap = ( props ) => {
+  const { apiKey, latitude, longitude } = props;
+  return (
+    <iframe
+      width="600"
+      height="450"
+      frameBorder="0" style={{ 'border': '0' }}
+      src={`https://www.google.com/maps/embed/v1/view?key=${apiKey}
+    &center=${latitude},${longitude}&zoom=12&maptype=satellite`} allowFullScreen>
+    </iframe>
+  );
+};
+
+export default GoogleMap;
