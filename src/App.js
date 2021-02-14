7c67c7dcab1e936bfff03054b973d7061fc8efbb
bennadler <bennadlerr@gmail.com>
2021-02-14 12:40:29 +0200

    app logic and structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import GoogleMap from './components/GoogleMap';
+import LocationCard from './components/LocationCard';
 
 function App() {
+
+  const apiKey = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk';
+  const ipDataKey = 'dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e';
+  const [city, setCity] = useState( '' );
+  const [country, setCountry] = useState( '' );
+  const [latitude, setLatitude] = useState( '' );
+  const [longitude, setLongitude] = useState( '' );
+
+  useEffect( () => {
+    let request = new XMLHttpRequest();
+    request.open( 'GET', `https://api.ipdata.co/?api-key=${ipDataKey}` );
+    request.setRequestHeader( 'Accept', 'application/json' );
+
+    request.onreadystatechange = function () {
+      if ( this.readyState === 4 && this.status == 200 ) {
+        const response = JSON.parse( this.response );
+        setCountry( response['country_name'] );
+        setCity( response['city'] );
+        setLatitude( response['latitude'] );
+        setLongitude( response['longitude'] )
+      }
+    };
+
+    request.send();
+  } );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Google Maps</h1>
+      <GoogleMap
+        apiKey={apiKey}
+        latitude={latitude}
+        longitude={longitude}
+      />
+      <LocationCard
+        city={city}
+        country={country} />
     </div>
   );
 }
